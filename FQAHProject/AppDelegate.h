09a49ba5be0ac040bee6e80bc812cb85df5486d2
Fqah <osmk@qq.com>
2016-03-31 17:08:41 +0800

    Initial Commit

---
+++ FQAHProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FQAHProject
+//
+//  Created by __无邪_ on 3/31/16.
+//  Copyright © 2016 fqah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FQAHProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FQAHProject
+//
+//  Created by __无邪_ on 3/31/16.
+//  Copyright © 2016 fqah. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

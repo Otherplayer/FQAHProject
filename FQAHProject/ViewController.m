bf79f67c0ea6e2bc52cbaf0d35534b23afd40c28
Fqah <osmk@qq.com>
2016-04-01 14:03:17 +0800

    f

--- FQAHProject/ViewController.m
+++ FQAHProject/ViewController.m
@@ -17,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [[HYQNetworkingManager sharedManager] getLaunchAdvertisementCompletedHandler:^(BOOL success, NSString *errDesc, HYQBASEModel *model) {
+        
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
